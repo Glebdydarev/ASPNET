1fec42aa2c22ce35a6f795997ac2d8a8f673d6a0
Gleb Dudarev <glebdydarev@mail.ru>
2022-02-16 09:04:26 -0600

    done

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Testing.Models;
+
+namespace Testing.Controllers
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+        void UpdateProduct(Product product);
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
+        void InsertProduct(Product productToInsert);
+        public void DeleteProduct(Product product);
+    }
+
+}
